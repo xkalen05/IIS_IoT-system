421c784b47046f1ac74789d46619262b38e35edd
Kheneon <95099750+Kheneon@users.noreply.github.com>
2023-11-26 19:04:53 +0100

    feat: User data validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,8 @@ use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Exception;
 
 class UserController extends Controller
 {
@@ -39,6 +41,18 @@ class UserController extends Controller
      */
     public function create(Request $request): Application|Redirector|RedirectResponse|\Illuminate\Contracts\Foundation\Application
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:1|max:255',
+            'surname' => 'required|min:1|max:255',
+            'email' => 'required|min:1|max:255|email',
+            'role' => 'required',
+            'password' => 'required|min:8|max:255',
+        ]);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator);
+        }
+
         User::create([
             'name' => $request->input('name'),
             'surname' => $request->input('surname'),
@@ -52,27 +66,21 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function editUserByAdmin(Request $request): Application|Redirector|RedirectResponse|\Illuminate\Contracts\Foundation\Application
     {
-//        dd($request);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:1|max:255',
+            'surname' => 'required|min:1|max:255',
+            'email' => 'required|min:1|max:255|email',
+            'role' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator);
+        }
+
         DB::table('users')->where('id', '=', $request->input('user_id'))->update([
             'name' => $request->input('name'),
             'surname' => $request->input('surname'),
@@ -86,7 +94,16 @@ class UserController extends Controller
 
     public function editUserByUser(Request $request): Application|Redirector|RedirectResponse|\Illuminate\Contracts\Foundation\Application
     {
-        //dd($request);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:1|max:255',
+            'surname' => 'required|min:1|max:255',
+            'email' => 'required|min:1|max:255|email',
+        ]);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator);
+        }
+
         DB::table('users')->where('id', '=', $request->input('user_id'))->update([
             'name' => $request->input('name'),
             'surname' => $request->input('surname'),
@@ -99,9 +116,20 @@ class UserController extends Controller
 
     public function editPasswordByAdmin(Request $request): Application|Redirector|RedirectResponse|\Illuminate\Contracts\Foundation\Application
     {
-        DB::table('users')->where('id', '=', $request->input('user_id'))->update([
-            'password' => Hash::make($request->input('password'))
+        $validator = Validator::make($request->all(), [
+            'password' => 'required|min:8|max:255',
+            'user_id' => 'required'
         ]);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator);
+        }
+
+        DB::table('users')
+            ->where('id', '=', $request->input('user_id'))
+            ->update([
+                'password' => Hash::make($request->input('password'))
+            ]);
 
         return redirect(route('admin.users'))->with('success', 'Password for user ' . $request->input('name') . ' ' .
             $request->input('surname') .' was succesfully changed.');
@@ -110,6 +138,14 @@ class UserController extends Controller
     public function editPasswordByUser(Request $request)
     {
         // TODO zmena hesla pro aktualne prihlaseneho uzivatele nefunguje
+        $validator = Validator::make($request->all(), [
+            'password' => 'required|min:8|max:255',
+        ]);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator);
+        }
+
         dd($request);
         $newPassword = $request->input('password');
 
@@ -121,19 +157,17 @@ class UserController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id): Application|Redirector|RedirectResponse|\Illuminate\Contracts\Foundation\Application
     {
-        DB::table('users')->where('id', '=', $id)->delete();
+        try {
+            DB::table('users')
+                ->where('id', '=', $id)
+                ->delete();
+        }catch (Exception $e){
+            return redirect(route('admin.users'))->with('error', 'Could not remove user. Already does not exist or wrong ID');
+        }
         return redirect(route('admin.users'))->with('success', 'User was succesfully deleted');
     }
 
