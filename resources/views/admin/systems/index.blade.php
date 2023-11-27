78202409d2932ea503f42211bc64473642a09e07
Kheneon <95099750+Kheneon@users.noreply.github.com>
2023-11-27 01:13:38 +0100

    fix: admin show system and device state

--- resources/views/admin/systems/index.blade.php
+++ resources/views/admin/systems/index.blade.php
@@ -56,6 +56,7 @@
                                 <th scope="col">Description</th>
                                 <th scope="col">Owner</th>
                                 <th scope="col">Actions</th>
+                                <th scope="col">State</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -88,6 +89,11 @@
                                         <a href="{{route('admin.system.delete', $system->id)}}"
                                            class="btn btn-danger">Delete</a>
                                     </td>
+                                    @if($system->result === 0)
+                                        <td class="bg-danger text-center">ERROR</td>
+                                    @else
+                                        <td class="bg-success text-center">OK</td>
+                                    @endif
                                 </tr>
                             @endforeach
                             </tbody>
