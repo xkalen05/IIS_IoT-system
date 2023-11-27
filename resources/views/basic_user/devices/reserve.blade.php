1d48d1a728f9d45761907e6626fcba3d03543b93
Kheneon <95099750+Kheneon@users.noreply.github.com>
2023-11-27 15:14:06 +0100

    fix: Add device to system button adjustment

--- resources/views/basic_user/devices/reserve.blade.php
+++ resources/views/basic_user/devices/reserve.blade.php
@@ -5,7 +5,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="reserve_device_{{$system->id}}_modal">Reserve device</h1>
+                <h1 class="modal-title fs-5" id="reserve_device_{{$system->id}}_modal">Add device to the system</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -13,7 +13,7 @@
                     @csrf
                     <input type="hidden" name="system_id" value="{{ $system->id }}">
                     <div class="mb-3">
-                        <label for="user-checkbox">Select devices that you want to reserve:</label>
+                        <label for="user-checkbox">Select device that you want to add to your system:</label>
                         <div class="checkbox-dropdown">
                             <select multiple class="form-select" id="device_id" name="device_id">
                                 @foreach($devices_free as $device)
