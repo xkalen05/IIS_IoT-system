20cfbc475238286ad111437a75c066f176641d25
Kheneon <95099750+Kheneon@users.noreply.github.com>
2023-11-27 16:58:55 +0100

    fix: admin could not change users email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\System;
 use App\Models\User;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -20,6 +19,27 @@ use App\Traits\CheckEmail;
 class UserController extends Controller
 {
     use CheckEmail;
+    /**
+     * Checks if user param $id is identical with email
+     *
+     * @return true If email and id is one user or email is not used yet
+     * @return false If email is already in use by another user
+     */
+    function checkUserIDByEmail($email,$id):bool{
+        $uid = DB::table('users')
+            ->where('email','=',"$email")
+            ->select('id')
+            ->get();
+
+        if($uid->isEmpty()){
+            return true;
+        }
+
+        if($uid->first()->id === $id){
+            return true;
+        }
+        return false;
+    }
 
     /**
      * Display a listing of the resource.
@@ -47,7 +67,7 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|min:1|max:255',
             'surname' => 'required|min:1|max:255',
-            'email' => 'required|min:1|max:255|email',
+            'email' => 'required|min:1|max:255|email|unique:users',
             'role' => 'required',
             'password' => 'required|min:8|max:255',
         ]);
@@ -69,7 +89,7 @@ class UserController extends Controller
         ]);
 
         return redirect(route('admin.users'))->with('success', 'User ' . $request->input('name') . ' ' .
-            $request->input('surname') .' succesfully created.');
+            $request->input('surname') .' successfully created.');
     }
 
     /**
@@ -80,8 +100,9 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|min:1|max:255',
             'surname' => 'required|min:1|max:255',
-            'email' => 'required|min:1|max:255|email|unique:users',
+            'email' => 'required|min:1|max:255|email',
             'role' => 'required',
+            'user_id' => 'required',
         ]);
 
         if($validator->fails()){
@@ -90,6 +111,10 @@ class UserController extends Controller
 
         if(!$this->CheckEmailFunc($request->input('email'))){
             return redirect()->back()->with('error','email is in wrong format')->withInput();
+        }
+
+        if(!$this->checkUserIDByEmail($request->input('email'),$request->input('user_id'))){
+            return redirect(route('admin.users'))->with('error','Email is already used by another user');
         }
 
         DB::table('users')->where('id', '=', $request->input('user_id'))->update([
@@ -99,7 +124,7 @@ class UserController extends Controller
             'role' => $request->input('role'),
         ]);
 
-        return redirect(route('admin.users'))->with('success', 'Changes were succesfully saved');
+        return redirect(route('admin.users'))->with('success', 'Changes were successfully saved');
 
     }
 
@@ -109,6 +134,7 @@ class UserController extends Controller
             'name' => 'required|min:1|max:255',
             'surname' => 'required|min:1|max:255',
             'email' => 'required|min:1|max:255|email',
+            'user_id' => 'required'
         ]);
 
         if($validator->fails()){
@@ -119,13 +145,17 @@ class UserController extends Controller
             return redirect()->back()->with('error','email is in wrong format')->withInput();
         }
 
+        if(!$this->checkUserIDByEmail($request->input('email'),$request->input('user_id'))){
+            redirect()->back()->with('error','Email is already used by another user');
+        }
+
         DB::table('users')->where('id', '=', $request->input('user_id'))->update([
             'name' => $request->input('name'),
             'surname' => $request->input('surname'),
             'email' => $request->input('email'),
         ]);
 
-        return redirect(route('profile.index'))->with('success', 'Changes were succesfully saved');
+        return redirect(route('profile.index'))->with('success', 'Changes were successfully saved');
 
     }
 
@@ -147,12 +177,11 @@ class UserController extends Controller
             ]);
 
         return redirect(route('admin.users'))->with('success', 'Password for user ' . $request->input('name') . ' ' .
-            $request->input('surname') .' was succesfully changed.');
+            $request->input('surname') .' was successfully changed.');
     }
 
     public function editPasswordByUser(Request $request)
     {
-        // TODO zmena hesla pro aktualne prihlaseneho uzivatele nefunguje
         $validator = Validator::make($request->all(), [
             'password' => 'required|min:8|max:255',
         ]);
@@ -167,7 +196,7 @@ class UserController extends Controller
             'password' => Hash::make($newPassword)
         ]);
 
-        return redirect(route('profile.index'))->with('success', 'Password was succefully changed');
+        return redirect(route('profile.index'))->with('success', 'Password was successfully changed');
     }
 
     /**
@@ -194,7 +223,7 @@ class UserController extends Controller
         }catch (Exception $e){
             return redirect(route('admin.users'))->with('error', 'Could not remove user. Already does not exist or wrong ID');
         }
-        return redirect(route('admin.users'))->with('success', 'User was succesfully deleted');
+        return redirect(route('admin.users'))->with('success', 'User was successfully deleted');
     }
 
 
